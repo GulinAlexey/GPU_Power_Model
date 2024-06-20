e314fd82a24d62ef068c4c1f3ec21d3f92e6be7a
GulinAlexey <gulin-2001@mail.ru>
2024-06-20 23:45:04 +0700

    Доб. заготовка основного класса

---
+++ GPU_Power_Model.py
@@ -0,0 +1,12 @@
+import PySimpleGUI as sg #версия 5.0.5
+
+class Main:
+    def __init__(self):
+        pass
+
+    def mainLoop(self):
+        while True:
+            pass
+
+main = Main()
+main.mainLoop()

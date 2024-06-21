028021a1258f2e02fafecf5dfd074803293db6c3
GulinAlexey <gulin-2001@mail.ru>
2024-06-21 23:51:10 +0700

    Добавлен вывод графика мощности GPU

--- GPU_Power_Model.py
+++ GPU_Power_Model.py
@@ -1,4 +1,5 @@
 import csv
+import matplotlib.pyplot as plt #версия 3.9.0
 
 class Main:
     def __init__(self):
@@ -13,6 +14,7 @@ class Main:
             gpuPowerMin = float('Inf')
             gpuPowerMinLine = []
             paramNamesLine = []
+            print("Значения мощности GPU [W]:")
             for line in csvFile:
                 if line[6] != " GPU Power [W] ":
                     currentGpuPower = float(line[6])
@@ -25,13 +27,18 @@ class Main:
                 else:
                     paramNamesLine = line
 
-            print(f"Наименьшее значение мощности GPU: {gpuPowerMin} Вт")
+            if gpuPowerMin == float('Inf') or gpuPowerMin == 0:
+                print("Определить наименьшую мощность GPU не удалось")
+                break
+            print(f"\nНаименьшее значение мощности GPU: {gpuPowerMin} Вт")
             print("Показания сенсоров при минимальной мощности GPU:")
             i = 0
             for param in gpuPowerMinLine:
                 if len(param) > 0:
                     print(f"{paramNamesLine[i].strip()}: {param.strip()}")
                 i = i + 1
+            plt.plot(gpuPowerList)
+            plt.show()
             break
 
 

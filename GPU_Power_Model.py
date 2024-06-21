5974346b4d3ee32f5810d26f0b2fbf799b78379d
GulinAlexey <gulin-2001@mail.ru>
2024-06-21 19:36:07 +0700

    Добавлено нахождение мин. значения мощности в данных .csv, а также вывод показаний сенсоров при этом значении. Мощность должна быть больше нуля

--- GPU_Power_Model.py
+++ GPU_Power_Model.py
@@ -1,12 +1,39 @@
-import PySimpleGUI as sg #версия 5.0.5
+import csv
 
 class Main:
     def __init__(self):
-        pass
+        self.__inputFile = "GPU-Z Sensor Log.csv"
 
     def mainLoop(self):
         while True:
-            pass
+            file = open(self.__inputFile, mode='r')
+            csvFile = csv.reader(file)
+
+            gpuPowerList = []
+            gpuPowerMin = float('Inf')
+            gpuPowerMinLine = []
+            paramNamesLine = []
+            for line in csvFile:
+                if line[6] != " GPU Power [W] ":
+                    currentGpuPower = float(line[6])
+                    print(currentGpuPower)
+
+                    gpuPowerList.append(currentGpuPower)
+                    if currentGpuPower < gpuPowerMin and currentGpuPower != 0:
+                        gpuPowerMin = currentGpuPower
+                        gpuPowerMinLine = line
+                else:
+                    paramNamesLine = line
+
+            print(f"Наименьшее значение мощности GPU: {gpuPowerMin} Вт")
+            print("Показания сенсоров при минимальной мощности GPU:")
+            i = 0
+            for param in gpuPowerMinLine:
+                if len(param) > 0:
+                    print(f"{paramNamesLine[i].strip()}: {param.strip()}")
+                i = i + 1
+            break
+
 
 main = Main()
 main.mainLoop()

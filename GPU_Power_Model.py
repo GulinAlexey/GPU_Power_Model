0dee64d17a6e50684ba8afe0674e016b6dfb9985
GulinAlexey <gulin-2001@mail.ru>
2025-03-14 00:34:27 +0700

    Доб. уменьшение Power Limit после каждого теста
    А также запись в БД значений Power Limit и TDP. Тесты останавливаются после некорректной работы бенчмарка или pynvml, или после успешного достижения min Power Limit. Перед первым тестом устанавливается значение Power Limit по умолчанию

--- GPU_Power_Model.py
+++ GPU_Power_Model.py
@@ -56,8 +56,9 @@ class Main:
             fan_speed = pynvml.nvmlDeviceGetFanSpeed(self.__handle)
             clock_info = pynvml.nvmlDeviceGetClockInfo(self.__handle, pynvml.NVML_CLOCK_GRAPHICS)
             memory_clock = pynvml.nvmlDeviceGetClockInfo(self.__handle, pynvml.NVML_CLOCK_MEM) / 2
-            power_usage = pynvml.nvmlDeviceGetPowerUsage(self.__handle) / 1000.0  # В ваттах
-            power_limit = pynvml.nvmlDeviceGetEnforcedPowerLimit(self.__handle) / 1000.0  # В ваттах
+            power_usage = pynvml.nvmlDeviceGetPowerUsage(self.__handle)
+            power_limit = pynvml.nvmlDeviceGetEnforcedPowerLimit(self.__handle)
+            power_limit_constraints = pynvml.nvmlDeviceGetPowerManagementLimitConstraints(self.__handle)
         except Exception as e:
             # Обработка любых ошибок
             print(f"Произошло исключение {type(e).__name__}: {e}")  # Вывести название ошибки и сообщение
@@ -81,8 +82,10 @@ class Main:
             "Memory Used [MB]": memory_info.used / 1024 / 1024,
             "GPU Load [%]": util.gpu,
             "Memory Controller Load [%]": util.memory,
-            "Board Power Draw [W]": power_usage,
-            "Power Consumption [% TDP]": (power_usage / power_limit) * 100,
+            "Board Power Draw [W]": power_usage / 1000.0,  # В ваттах
+            "Power Consumption [% TDP]": (power_usage / power_limit_constraints[1]) * 100,
+            "Power Limit [W]": power_limit / 1000.0,  # В ваттах
+            "TDP Limit [%]": (power_limit / power_limit_constraints[1]) * 100,
             "GPU Voltage [V]": voltage
         }
         return gpu_data
@@ -102,6 +105,8 @@ class Main:
         print(f"Загрузка контроллера памяти: {gpu_data['Memory Controller Load [%]']}%")
         print(f"Потребление платы: {gpu_data['Board Power Draw [W]']} W")
         print(f"Потребление энергии: {gpu_data['Power Consumption [% TDP]']}% TDP")
+        print(f"Ограничение мощности: {gpu_data['Power Limit [W]']} W")
+        print(f"Ограничение TDP: {gpu_data['TDP Limit [%]']} %")
         print(f"Напряжение GPU: {gpu_data['GPU Voltage [V]']} V")
         print("=" * 50)
 
@@ -189,6 +194,39 @@ class Main:
         if not self.__check_benchmark_log_for_normal_shutdown(): # Проверка, что работа бенчмарка была завершена корректно
             return False
 
+    # Метод вывода данных о TDP и Power Limit
+    def __print_tdp_info(self):
+        power_limit = pynvml.nvmlDeviceGetEnforcedPowerLimit(self.__handle)
+        power_limit_default = pynvml.nvmlDeviceGetPowerManagementDefaultLimit(self.__handle)
+        power_limit_constraints = pynvml.nvmlDeviceGetPowerManagementLimitConstraints(self.__handle)
+
+        print(f"Текущий Power Limit: {power_limit / 1000} [W]")
+        print(f"Текущий TDP Limit: {(power_limit / power_limit_constraints[1]) * 100} %")
+        print(f"Power Limit по умолчанию: {power_limit_default / 1000} [W]")
+        print(f"Ограничения Power Limit: {power_limit_constraints[0] / 1000} W - {power_limit_constraints[1] / 1000} [W]")  # max Power Limit = 100% TDP
+        print(f"Ограничения TDP: {(power_limit_constraints[0] / power_limit_constraints[1]) * 100} % - 100 %")
+
+    # Метод уменьшения Power Limit GPU для прохождения следующего теста бенчмарка
+    def __reduce_tdp(self, milliwatt_reducing_value):
+        # Получение текущего Power Limit (в абсолютных величинах, а не проценты TDP)
+        power_limit = pynvml.nvmlDeviceGetEnforcedPowerLimit(self.__handle)
+        power_limit_constraints = pynvml.nvmlDeviceGetPowerManagementLimitConstraints(self.__handle)
+
+        # Уменьшение TDP
+        new_power_limit = max(power_limit_constraints[0], power_limit - milliwatt_reducing_value)  # Уменьшить на X мВт
+        pynvml.nvmlDeviceSetPowerManagementLimit(self.__handle, new_power_limit)
+
+        power_limit = pynvml.nvmlDeviceGetPowerManagementLimit(self.__handle)
+        print(f"Новый Power Limit: {power_limit / 1000} W")
+        print(f"Новое ограничение TDP: {power_limit / power_limit_constraints[1]} %")
+        return power_limit
+
+    # Вернуть значение Power Limit GPU по умолчанию
+    def __set_tdp_to_default(self):
+        default_power_limit = pynvml.nvmlDeviceGetPowerManagementDefaultLimit(self.__handle)
+        pynvml.nvmlDeviceSetPowerManagementLimit(self.__handle, default_power_limit)
+        return default_power_limit
+
     def main_loop(self):
         collection = self.__db["gpu_data" + " " + datetime.now().strftime("%Y-%m-%d %H:%M:%S")]  # Название коллекции
 
@@ -197,13 +235,29 @@ class Main:
         time_test_running = 30
         time_after_finish_test = 10
 
-        # Один запуск теста бенчмарка со сбором данных в MongoDB (ограниченный по времени)
-        res = self.__run_benchmark(collection, self.__benchmark_start_command, time_before_start_test, time_test_running, time_after_finish_test)
-        if res is False:
-            print("Работа теста бенчмарка остановлена. Данные параметры работы GPU являются нестабильными")
-            return
-        # Запись FPS из файла лога MSI Kombustor (и эффективность [FPS/W]) в соответствующие документы коллекции MongoDB
-        self.__update_fps_and_efficiency_in_collection(self.__benchmark_log_path, collection)
+        watt_reducing_value = 5 # Величина уменьшения Power Limit за один тест (в W)
+        milliwatt_reducing_value = watt_reducing_value * 1000
+
+        current_power_limit = self.__set_tdp_to_default()  # Вернуть значение Power Limit GPU по умолчанию
+        previous_power_limit = None
+        self.__print_tdp_info()  # Вывод данных о TDP и Power Limit
+        # Цикл андервольтинга и тестирования
+        while True:
+            # Один запуск теста бенчмарка со сбором данных в MongoDB (ограниченный по времени)
+            res = self.__run_benchmark(collection, self.__benchmark_start_command, time_before_start_test, time_test_running, time_after_finish_test)
+            if res is False:
+                print("Работа теста бенчмарка была остановлена. Данные параметры работы GPU являются нестабильными")
+                print(f"Нестабильное значение Power Limit: {current_power_limit / 1000} W")
+                print(f"Стабильное значение Power Limit: {previous_power_limit / 1000} W")
+                return
+            # Запись FPS из файла лога MSI Kombustor (и эффективность [FPS/W]) в соответствующие документы коллекции MongoDB
+            self.__update_fps_and_efficiency_in_collection(self.__benchmark_log_path, collection)
+            # Уменьшить Power Limit GPU для прохождения следующего теста бенчмарка
+            previous_power_limit = current_power_limit
+            current_power_limit = self.__reduce_tdp(milliwatt_reducing_value)
+            if current_power_limit == previous_power_limit:
+                print(f"Минимальное значение Power Limit: {current_power_limit / 1000} W достигнуто, все возможные тесты пройдены")
+                return
 
 
 main = Main()

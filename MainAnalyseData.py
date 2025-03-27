778dcb9ff5371b72069281874ca0c91932c2015a
GulinAlexey <gulin-2001@mail.ru>
2025-03-27 20:26:16 +0700

    Вычисл. коэф. корреляции вынесено в отдельный метод
    Добавлены коэффициенты Кендалла и Спирмена

--- MainAnalyseData.py
+++ MainAnalyseData.py
@@ -6,19 +6,16 @@ class MainAnalyseData:
     def __init__(self):
         self.__client = pymongo.MongoClient("mongodb://localhost:27017/")  # Адрес сервера MongoDB
         self.__db = self.__client["gpu_benchmark_monitoring"]  # Название базы данных
-        self.__collection = self.__db["gpu_benchmark_monitoring.gpu_data 2025-03-25 01:01:34"] # TODO пример конкретной коллекции
+        # Список всех коллекций с данными в БД
+        self.__collections = []
+        for collection_name in self.__db.list_collection_names():
+            self.__collections.append(self.__db[collection_name])
 
-    def main_loop(self):
-        # Получить все документы из коллекции
-        documents = list(self.__collection.find())
-        # Преобразовать данные в DataFrame
-        df = pd.DataFrame(documents)
-        print(f"Всего {len(df)} документов")
-        # Оставить только те документы, где FPS не пустой (и не None)
-        df = df[df["FPS"].notna()]
-        print(f"Всего {len(df)} документов с FPS")
+    @staticmethod
+    def __correlation_coefficient(df, method='pearson'):
         # Выбрать параметры для анализа
-        correlation_columns = ["Power Limit [W]", "GPU Clock Frequency Offset [MHz]", "Memory Clock Offset [MHz]", "FPS"]
+        correlation_columns = ["Power Limit [W]", "GPU Clock Frequency Offset [MHz]", "Memory Clock Offset [MHz]",
+                               "FPS"]
         # Оставить только нужные столбцы
         df = df[["Benchmark test type"] + correlation_columns]
         # Определить корреляцию для каждого Benchmark test type
@@ -30,14 +27,32 @@ class MainAnalyseData:
             benchmark_data = df[df["Benchmark test type"] == benchmark]
             if len(benchmark_data) > 1:  # Проверить, чтобы было больше 1 значения для расчета корреляции
                 # Вычислить корреляцию между FPS и другими столбцами
-                correlation_matrix = benchmark_data[correlation_columns].corr()
+                correlation_matrix = benchmark_data[correlation_columns].corr(method)
                 fps_correlation = correlation_matrix["FPS"]  # Взять корреляции FPS с остальными параметрами
                 correlations[benchmark] = fps_correlation.drop("FPS").to_dict()
         # Вывести результаты
+        print("=" * 50)
+        print("Correlation coefficient " + method)
         for benchmark, correlation in correlations.items():
             print(f"Benchmark test type: {benchmark}")
             for param, corr_value in correlation.items():
                 print(f"  {param}: {corr_value:.4f}")
+
+    def main_loop(self):
+        # Получить все документы из коллекции
+        all_documents = []
+        for collection in self.__collections:
+            documents = list(collection.find())
+            all_documents.extend(documents)  # Добавить документы в общий список
+        # Преобразовать данные в DataFrame
+        df = pd.DataFrame(all_documents)
+        print(f"Всего {len(df)} документов")
+        # Оставить только те документы, где FPS не пустой (и не None)
+        df = df[df["FPS"].notna()]
+        print(f"Всего {len(df)} документов с FPS")
+        self.__correlation_coefficient(df, 'pearson')
+        self.__correlation_coefficient(df, 'kendall')
+        self.__correlation_coefficient(df, 'spearman')
 
 
 main = MainAnalyseData()

c1a75b158433ef80ac2fb236ecdd477a4a963265
GulinAlexey <gulin-2001@mail.ru>
2025-05-12 01:14:19 +0700

    Начало работы над вызовом через сокеты системы анализа данных

--- DataAnalysisSystem.py
+++ DataAnalysisSystem.py
@@ -1,6 +1,69 @@
+import socket
+import threading
+from SocketCalls import SocketCalls
+
+
 class DataAnalysisSystem:
     def __init__(self):
-        pass
+        self.__address = SocketCalls.DATA_ANALYSIS_SYSTEM_ADDRESS
+        self.__port = SocketCalls.DATA_ANALYSIS_SYSTEM_PORT
+
+    # Обработка вызова метода через сокеты
+    def __handle_client(self, client_socket):
+        try:
+            # Чтение и декодирование запроса
+            request = client_socket.recv(1024).decode('utf-8')
+            print(f"Получено: {request}")
+            # Разбиение запроса на метод и параметры
+            parts = request.split(',')
+            method_name = parts[0].strip()
+            parameters = [p.strip() for p in parts[1:] if p.strip()]  # Убрать пустые значения и пробелы
+            # Вызов соответствующего метода
+            if method_name == "": #TODO
+                if parameters:
+                    response = f"Для метода {method_name} параметры не требуются"
+                    print(response)
+                else:
+                    response = self.__() #TODO
+            elif method_name == "": #TODO
+                if len(parameters) != 1:
+                    response = f"Метод {method_name} требует 1 параметр"
+                    print(response)
+                else:
+                    offset = int(parameters[0])
+                    response = self.__(offset) #TODO
+            else:
+                response = f"Неизвестный метод {method_name}"
+                print(response)
+            # Преобразование response в строку, если оно является типа bool или int
+            if isinstance(response, (bool, int)):
+                response = str(response)
+            elif isinstance(response, tuple):
+                # Преобразовать кортеж в строку
+                response = ', '.join(map(str, response))
+            elif response is None:
+                response = "None"
+            # Отправка ответа клиенту
+            client_socket.send(response.encode('utf-8'))
+        except Exception as e:
+            print(f"Ошибка обработки клиента: {e}")
+            response = "Ошибка сервера"
+            client_socket.send(response.encode('utf-8'))
+        finally:
+            # Закрытие соединения
+            client_socket.close()
 
     def run(self):
-        pass
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((self.__address, self.__port))
+        server.listen(5)
+        print("Сервер системы анализа данных для построения модели питания GPU запущен и ожидает подключения клиентов...")
+        while True:
+            client_socket, addr = server.accept()
+            print(f"Подключен клиент: {addr}")
+            client_handler = threading.Thread(target=self.__handle_client, args=(client_socket,))
+            client_handler.start()
+
+
+system = DataAnalysisSystem()
+system.run()
